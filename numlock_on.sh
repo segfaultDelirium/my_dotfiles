3ea52e60e9595cf47fca00bbafa3d74cee43269c
segfault_delirium <example@mail.com>
2023-10-19 15:54:51 +0200

    add scripts and konsave config

---
+++ numlock_on.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/usr/bin/numlockx on && xmodmap -e 'keycode 77 = NoSymbol Num_Lock' 
---
+++ taurus_socks.sh
@@ -0,0 +1,2 @@
+!/bin/bash
+ssh -o "ServerAliveInterval 60" -D 9091 -X 9urban@taurus.fis.agh.edu.pl
